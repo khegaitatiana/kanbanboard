051988b688fe4675fd09490229f3eb09d2ebb149
Tatiana Khegai <tatiana.khegai@olc.cz>
2019-04-13 16:57:32 +0200

    re #1: - code beautify - rename namespaces, classes - add bootstrap css

--- src/public/index.php
+++ src/public/index.php
@@ -1,19 +1,26 @@
 <?php
-use KanbanBoard\Authentication;
-use KanbanBoard\GithubActual;
-use KanbanBoard\Utilities;
 
-require '../classes/KanbanBoard/Github.php';
-require '../classes/Utilities.php';
-require '../classes/KanbanBoard/Authentication.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Utilities\Utilities;
+use KanbanBoard\Authentication;
+use KanbanBoard\Github;
+use KanbanBoard\Application;
+
+#TODO move to .env
+include 'credentials.php';
+putenv("GH_CLIENT_ID=" . $GH_CLIENT_ID);
+putenv("GH_CLIENT_SECRET=" . $GH_CLIENT_SECRET);
+putenv("GH_ACCOUNT=" . $GH_ACCOUNT);
+putenv("GH_REPOSITORIES=" . $GH_REPOSITORIES);
 
 $repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
-$authentication = new \KanbanBoard\Login();
+$authentication = new Authentication();
 $token = $authentication->login();
-$github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
-$board = new \KanbanBoard\Application($github, $repositories, array('waiting-for-feedback'));
+$github = new Github($token, Utilities::env('GH_ACCOUNT'));
+$board = new Application($github, $repositories, array('waiting-for-feedback'));
 $data = $board->board();
 $m = new Mustache_Engine(array(
-	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
+    'loader' => new Mustache_Loader_FilesystemLoader('../views'),
 ));
 echo $m->render('index', array('milestones' => $data));
